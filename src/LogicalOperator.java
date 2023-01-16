228605c7491f5893d5a03a8d0ea793a4443dd3d5
mehedi-01 <mehedihasan376766@gmail.com>
2023-01-16 20:04:43 +0600

    Initial version

--- src/LogicalOperator.java
+++ src/LogicalOperator.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class LogicalOperator {
     public static void main(String[] args) {
 
+        Scanner input =  new Scanner(System.in);
+
+        System.out.println("Enter Any Letter: ");
+
+        char ch = input.next().charAt(0);
+
+        if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("Vowel");
+        }
     }
 }

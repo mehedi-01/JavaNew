bc5ba57be3b97c52bf0a6cf17ecc2c7db1d3bf3b
mehedi-01 <mehedihasan376766@gmail.com>
2023-01-21 19:53:44 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/BitwiseOperator.java
@@ -0,0 +1,24 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+         int a = 32;
+
+         int b  = 12;
+
+         int c;
+
+         c = a & b;
+        System.out.println("a & b = "+c);
+
+        c = a | b;
+        System.out.println("a | b = "+c);
+
+        c = a^b;
+        System.out.println("a ^ b = "+c);
+
+        c = a >> 3;
+        System.out.println("a >> 3 = "+c);
+
+        c = a << 3;
+        System.out.println("a << 3 = "+c);
+    }
+}

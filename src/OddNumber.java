2f55b587f881c79e2af3f683ac3896fc4dfddaee
mehedi-01 <mehedihasan376766@gmail.com>
2023-01-23 00:00:11 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/OddNumber.java
@@ -0,0 +1,9 @@
+public class OddNumber {
+    public static void main(String[] args) {
+    int sum = 0;
+        for(int i = 1; i<=100;i=i+2){
+            sum= sum+i;
+        }
+        System.out.println(sum);
+    }
+}

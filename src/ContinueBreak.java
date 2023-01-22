bf46221efae40bbd5a6d82aacebab80f1c72a042
mehedi-01 <mehedihasan376766@gmail.com>
2023-01-22 23:28:36 +0600

    Merge remote-tracking branch 'origin/master'

--- src/ContinueBreak.java
+++ src/ContinueBreak.java
@@ -1,2 +1,14 @@
 public class ContinueBreak {
+
+    public static void main(String[] args) {
+        for (int i= 1; i<=100; i=i+3){
+            if(i==10){
+                continue;
+            }
+            if(i>13){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
 }

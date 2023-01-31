b4c453e82ba3b1937f219fb29c7a255e45cb7825
mehedi-01 <mehedihasan376766@gmail.com>
2023-01-31 23:24:07 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/array_demo/Person.java
@@ -0,0 +1,20 @@
+package array_demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public  class Person {
+
+   private List<String> names = new ArrayList<>();
+
+    public Person() {
+        this.names.add("ROny");
+        this.names.add("ROny1");
+        this.names.add("ROny2");
+        this.names.add("ROny3");
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+}

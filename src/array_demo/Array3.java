47b45182fa40ce4a2b1a5d700e88bde50e35a540
mehedi-01 <mehedihasan376766@gmail.com>
2023-01-31 23:24:07 +0600

    Merge remote-tracking branch 'origin/master'

--- src/array_demo/Array3.java
+++ src/array_demo/Array3.java
@@ -1,16 +1,31 @@
 package array_demo;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Array3 {
     public static void main(String[] args) {
 
-        int[] num = {12, 12, 44, 34, 23,78,14,45,78};
+/*        int[] num = {12, 12, 44, 34, 23,78,14,45,78};
         int sum = 0;
         for (int x: num){
             sum = sum+x;
 
 
         }
-        System.out.println(sum);
+        System.out.println(sum);*/
+
+        Person person = new Person();
+        for(String s: person.getNames())
+        {
+            System.out.println(s);
+        }
+
     }
+
 }
+
+
+
+

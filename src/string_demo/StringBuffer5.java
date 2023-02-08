5a8c140cc4a875adef25b523bbdb07e8246894e6
mehedi-01 <mehedihasan376766@gmail.com>
2023-02-08 22:59:01 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/string_demo/StringBuffer5.java
@@ -0,0 +1,13 @@
+package string_demo;
+
+public class StringBuffer5 {
+    public static void main(String[] args) {
+        StringBuffer sad = new StringBuffer("Nayeem Hasan");
+        System.out.println(sad);
+
+        sad.reverse();
+        System.out.println(sad);
+
+
+    }
+}
---
+++ src/string_demo/StringBuilder6.java
@@ -0,0 +1,10 @@
+package string_demo;
+
+public class StringBuilder6 {
+    public static void main(String[] args) {
+
+        StringBuilder str = new StringBuilder();
+
+        System.out.println("str = "+ str);
+    }
+}

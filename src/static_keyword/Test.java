9ae0b5e07ee6c9c286c6aa6dd6ef92c770559f9d
mehedi-01 <mehedihasan376766@gmail.com>
2023-02-21 19:34:56 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/static_keyword/Test.java
@@ -0,0 +1,15 @@
+package static_keyword;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Hasan",1235);
+        Student s2 = new Student("Han",1535);
+        s1.displayInfo();
+        s2.displayInfo();
+
+
+
+    }
+}

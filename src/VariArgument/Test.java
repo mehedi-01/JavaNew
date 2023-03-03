8bf4d74474be9e488fdf8e9a0017ceae27cb4a04
mehedi-01 <mehedihasan376766@gmail.com>
2023-03-03 17:18:14 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/VariArgument/Test.java
@@ -0,0 +1,12 @@
+package VariArgument;
+
+public class Test {
+
+    public static void main(String[] args) {
+        VariableArguments obj = new VariableArguments();
+        obj.add(10,20);
+        obj.add(10,20,30);
+        obj.add(10,20,30,40);
+
+    }
+}
